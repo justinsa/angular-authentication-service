e2f5a38ad00806d0c3f3811fa2efee27ca65b443
justinsa <justinsa@live.ca>
2014-06-04 15:00:01 -0700

    Remove nonsense tests

--- spec/controllers.js
+++ spec/controllers.js
@@ -3,15 +3,8 @@
 describe('controllers', function() {
   beforeEach(module('authentication.service'));
   describe('SecurityController', function() {
-    var scope;
-    var ctrl;
-
     it('should have a permit function', function() {
-      scope.permit.should.be.a.Function;
-    });
-
-    it('should define a set of controls', function() {
-      _.keys(scope.controls).length.should.equal(2);
+      // TODO
     });
   });
 });
