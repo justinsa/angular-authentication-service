d60564ec5f4ba54b73d80a7bc5b2cfefba04f713
justinsa <justinsa@live.ca>
2014-06-04 10:07:42 -0700

    Use proper test structure for client-side code

--- spec/controllers.js
+++ spec/controllers.js
@@ -1,28 +1,17 @@
 'use strict';
 
-require('should');
-global._ = require('underscore');
-global._.str = require('underscore.string');
-var sinon = require('sinon');
-
 describe('controllers', function() {
-  describe('security', function() {
-    var securityController = require('../angular-authentication-service.js');
-
-    it('should have a configure function', function() {
-      securityController.configure.should.be.a.Function;
-    });
+  beforeEach(module('authentication.service'));
+  describe('SecurityController', function() {
+    var scope;
+    var ctrl;
 
     it('should have a permit function', function() {
-      securityController.permit.should.be.a.Function;
+      scope.permit.should.be.a.Function;
     });
 
-    it('should error when given a bad email address', function() {
-      var req = { body: { email: 'bad@email', firstName: 'first', lastName: 'last', phone: '5555555555', postalCode: '55555'} };
-      var res = { json: sinon.spy() };
-
-      securityController.post.lead(req, res);
-      res.json.calledWith(400).should.be.true;
-    })
+    it('should define a set of controls', function() {
+      _.keys(scope.controls).length.should.equal(2);
+    });
   });
 });
