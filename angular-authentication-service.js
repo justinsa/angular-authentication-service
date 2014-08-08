7297c0ec00d6e3b92ed6ee296a968d471ef7a2fe
Josh Mackey <josh@mackey.me>
2014-08-07 18:53:48 -0700

    Revert "Wrapped reauth function so it can update the profile."
    This reverts commit 57bc671e87ab05f409fb2567130c0a8e6f92a4a5.

--- angular-authentication-service.js
+++ angular-authentication-service.js
@@ -23,7 +23,7 @@
         return !_.isEmpty(userRoles) && !_.isEmpty(allowedRoles) &&
           (_.find(allowedRoles, function (role) { return _.contains(userRoles, role); }) !== undefined);
       },
-      reauthFunc: function (profile, callback) { callback(); },
+      reauthFunc: function () {},
       reauthTimeout: 1200000,
       reauthId: null
     };
@@ -35,15 +35,6 @@
     this.$get = [
     '$cookieStore', '$document', '$location', '$rootScope', '$store',
     function ($cookieStore, $document, $location, $rootScope, $store) {
-
-      var reauthWrapper = function() {
-        configuration.reauthFunc(authFunctions.profile(), function(updatedProfile) {
-          if (_.isObject(updatedProfile)) {
-            $store.set(configuration.profileStorageKey, updatedProfile);
-          }
-        });
-      };
-
       var authFunctions = {
         /**
          * returns true if there is a user profile in storage.
@@ -82,7 +73,7 @@
          */
         loginConfirmed: function (data) {
           $store.set(configuration.profileStorageKey, data);
-          configuration.reauthId = setInterval(reauthWrapper, configuration.reauthTimeout);
+          configuration.reauthId = setInterval(configuration.reauthFunc, configuration.reauthTimeout);
           $rootScope.$broadcast('event:auth-loginConfirmed', data);
         },
 
@@ -178,8 +169,8 @@
          */
         reauth: function() {
           if (authFunctions.isAuthenticated()) {
-            reauthWrapper();
-            configuration.reauthId = setInterval(configuration.reauthWrapper, configuration.reauthTimeout);
+            configuration.reauthFunc();
+            configuration.reauthId = setInterval(configuration.reauthFunc, configuration.reauthTimeout);
           }
         }
       };
